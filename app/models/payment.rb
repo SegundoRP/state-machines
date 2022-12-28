4dad3961b73a6798b828a4bfb367abfa8800911e
Segundo Rebaza <alvaro.rp1593@gmail.com>
2022-12-28 00:02:36 -0500

    set other states

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,7 +1,19 @@
 class Payment < ApplicationRecord
   state_machine :state, initial: :pending do
     event :process do
-      transition pending: :process
+      transition pending: :processing
+    end
+
+    event :fail do
+      transition processing: :failed
+    end
+
+    event :success do
+      transition processing: :successful
+    end
+
+    event :refund do
+      transition successful: :refunded
     end
   end
 end
